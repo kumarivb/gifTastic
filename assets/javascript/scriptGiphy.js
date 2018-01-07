2e025c97456c41a3cce16690661eb005ddfa24e6
kvb <kumarivb@gmail.com>
2018-01-06 19:02:15 -0600

    added containers and a form to index.html

---
+++ assets/javascript/scriptGiphy.js
@@ -0,0 +1,34 @@
+// make sure JavaScript doesn't run until the HTML has finished loading
+$(document).ready(function() {
+
+// Variables ==========================================================
+var authKey = "in1rFgH6d3WCWcNBv1w2yNuXb8nxnNXF";
+var queryURL = ""
+
+
+// Functions and processes ============================================
+
+
+
+
+// HTML ===============================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
